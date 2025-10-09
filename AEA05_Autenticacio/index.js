a032ef9c2149976bb0daaf914d190e0228af7ab8
MarcBonamusa <marc.bonamusa.7e6@itb.cat>
2025-10-09 16:53:41 +0200

    login hecho

--- AEA05_Autenticacio/index.js
+++ AEA05_Autenticacio/index.js
@@ -34,6 +34,33 @@ app.post('/register', async (req, res) => {
 
 })
 
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    console.log('llego aqui')
+    const user = await UserRepository.login(username, password)
+    console.log('llego aqui 1')
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      SECRET_JWT_KEY,
+      { expiresIn: '1h' }
+    )
+    console.log('llego aqui 2')
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // la cookie solo se puede acceder en el servidor, no podrem fer un document.cookie
+        // secure: true, // la cookie solo funciona en https
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict', // la cookie es pot accedir dins del domini
+        maxAge: 1000 * 60 * 60 // la cookie te un temps de validesa d'una hora
+      })
+      .send({ user, token })
+  } catch (error) {
+    // 401 = no autoritzacio
+    res.status(401).send(error.message)
+  }
+})
+
 app.get('/protected', (req, res) => {
      res.render('protected')
 })
